462561a6bae1991dbe305f09fe4adda2a7c979e4
yixinglu <2520865+yixinglu@users.noreply.github.com>
2020-12-05 11:22:08 +0800

    Record tostring

--- result_set.go
+++ result_set.go
@@ -384,12 +384,12 @@ func (record Record) GetValueByColName(colName string) (*ValueWrapper, error) {
 	return record._record[index], nil
 }
 
-func (record Record) PrintRow() {
+func (record Record) String() string {
 	var strList []string
 	for _, val := range record._record {
 		strList = append(strList, val.String())
 	}
-	fmt.Printf(strings.Join(strList, ", "))
+	return strings.Join(strList, ", ")
 }
 
 func (record Record) hasColName(colName string) bool {
