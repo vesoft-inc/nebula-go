16617df25492fd023baabeb09122386594693082
Harris.Chu <1726587+HarrisChu@users.noreply.github.com>
2021-10-26 17:21:15 +0800

    check all the addresses in pool initialization (#146)
    * check all the addresses in pool initialization
    
    * format code

--- connection_pool.go
+++ connection_pool.go
@@ -68,6 +68,10 @@ func NewSslConnectionPool(addresses []HostAddress, conf PoolConfig, sslConfig *t
 
 // initPool initializes the connection pool
 func (pool *ConnectionPool) initPool() error {
+	if err := pool.checkAddresses(); err != nil {
+		return fmt.Errorf("failed to open connection, error: %s ", err.Error())
+	}
+
 	for i := 0; i < pool.conf.MinConnPoolSize; i++ {
 		// Simple round-robin
 		newConn := newConnection(pool.addresses[i%len(pool.addresses)])
@@ -343,4 +347,17 @@ func (pool *ConnectionPool) timeoutConnectionList() (closing []*connection) {
 		}
 	}
 	return
+}
+
+func (pool *ConnectionPool) checkAddresses() error {
+	var timeout = 3 * time.Second
+	if pool.conf.TimeOut != 0 && pool.conf.TimeOut < timeout {
+		timeout = pool.conf.TimeOut
+	}
+	for _, address := range pool.addresses {
+		if err := pool.Ping(address, timeout); err != nil {
+			return err
+		}
+	}
+	return nil
 }
