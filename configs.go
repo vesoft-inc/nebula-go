86a2cfc484754c19705e9df62f246ca77a3726d5
Harris.Chu <1726587+HarrisChu@users.noreply.github.com>
2021-12-27 19:32:25 +0800

    get default ssl config (#176)
    * get default ssl config
    
    * fix
    
    * add more meaningful comment

--- configs.go
+++ configs.go
@@ -9,6 +9,11 @@
 package nebula_go
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
 )
 
@@ -54,4 +59,49 @@ func GetDefaultConf() PoolConfig {
 		MaxConnPoolSize: 10,
 		MinConnPoolSize: 0,
 	}
+}
+
+// GetDefaultSSLConfig reads the files in the given path and returns a tls.Config object
+func GetDefaultSSLConfig(rootCAPath, certPath, privateKeyPath string) (*tls.Config, error) {
+	rootCA, err := openAndReadFile(rootCAPath)
+	if err != nil {
+		return nil, err
+	}
+	cert, err := openAndReadFile(certPath)
+	if err != nil {
+		return nil, err
+	}
+	privateKey, err := openAndReadFile(privateKeyPath)
+	if err != nil {
+		return nil, err
+	}
+	clientCert, err := tls.X509KeyPair(cert, privateKey)
+	if err != nil {
+		return nil, err
+	}
+	// parse root CA pem and add into CA pool
+	// for self-signed cert, use the local cert as the root ca
+	rootCAPool := x509.NewCertPool()
+	ok := rootCAPool.AppendCertsFromPEM(rootCA)
+	if !ok {
+		return nil, fmt.Errorf("unable to append supplied cert into tls.Config, please make sure it is a valid certificate")
+	}
+	return &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      rootCAPool,
+	}, nil
+}
+
+func openAndReadFile(path string) ([]byte, error) {
+	// open file
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open file %s: %s", path, err)
+	}
+	// read file
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, fmt.Errorf("unable to ReadAll file %s: %s", path, err)
+	}
+	return b, nil
 }
