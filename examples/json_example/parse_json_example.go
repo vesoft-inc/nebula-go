21b65afe97ee0956b553ddd3c26bb6927754fa52
Sophie <84560950+Sophie-Xie@users.noreply.github.com>
2023-06-13 14:21:29 +0800

    Cherry pick from 3.4-beta-sc:  support http2 (#278)
    * feat: support http2 (#246)
    
    * update fbthrift (#255)
    
    * fix conflicts
    
    * remove ssl examples
    
    ---------
    
    Co-authored-by: Vee Zhang <veezhang@126.com>

--- examples/json_example/parse_json_example.go
+++ examples/json_example/parse_json_example.go
@@ -23,6 +23,7 @@ const (
 	port     = 3699
 	username = "root"
 	password = "nebula"
+	useHTTP2 = false
 )
 
 // Initialize logger
@@ -77,6 +78,7 @@ func main() {
 	hostList := []nebula.HostAddress{hostAddress}
 	// Create configs for connection pool using default values
 	testPoolConfig := nebula.GetDefaultConf()
+	testPoolConfig.UseHTTP2 = useHTTP2
 
 	// Initialize connection pool
 	pool, err := nebula.NewConnectionPool(hostList, testPoolConfig, log)
