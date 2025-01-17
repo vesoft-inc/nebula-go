5312ccfebe2f22f066375861fba86745a420a21b
hayson <hayson97@qq.com>
2025-01-17 13:49:48 +0800

    feat: export getLocalTimeWithTimezoneName (#360)
    Co-authored-by: Anqi <16240361+Nicole00@users.noreply.github.com>

--- result_set.go
+++ result_set.go
@@ -1066,7 +1066,7 @@ func (t TimeWrapper) getLocalTimeWithTimezoneOffset(timezoneOffsetSeconds int32)
 		Microsec: int32(localTime.Nanosecond() / 1000)}, nil
 }
 
-// getLocalTimeWithTimezoneName returns a nebula.Time object
+// GetLocalTimeWithTimezoneName returns a nebula.Time object
 // representing local time using user specified timezone name.
 // Year, month, day in time.Time are filled with 0.
 //
@@ -1075,7 +1075,7 @@ func (t TimeWrapper) getLocalTimeWithTimezoneOffset(timezoneOffsetSeconds int32)
 //
 // Otherwise, the name is taken to be a location name corresponding to a file
 // in the IANA Time Zone database, such as "America/New_York".
-func (t TimeWrapper) getLocalTimeWithTimezoneName(timezoneName string) (*nebula.Time, error) {
+func (t TimeWrapper) GetLocalTimeWithTimezoneName(timezoneName string) (*nebula.Time, error) {
 	// Original time object generated from server in UTC
 	// Year, month and day are mocked up to fill the parameters
 	rawTime := time.Date(2020,
