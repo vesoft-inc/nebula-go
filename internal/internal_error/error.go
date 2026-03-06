c334e9b0e383fdca4486080ad09b07884c7e6afc
Xin Hao <haoxinst@gmail.com>
2026-03-06 13:58:15 +0800

    build: run the go fix cmd

--- internal/internal_error/error.go
+++ internal/internal_error/error.go
@@ -3,9 +3,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,15 +23,15 @@ import (
 func ErrAddressNotValid(address string, msg string) error {
 	var (
 		format string
-		args   []interface{}
+		args   []any
 	)
 
 	if msg == "" {
 		format = "address %s is not valid"
-		args = []interface{}{address}
+		args = []any{address}
 	} else {
 		format = "address %s is not valid, %s"
-		args = []interface{}{address, msg}
+		args = []any{address, msg}
 	}
 	return errors.NewNebulaError(errors.ERROR_ADDRESS_NOT_VALID, format, args...)
 }
--- pkg/errors/error.go
+++ pkg/errors/error.go
@@ -3,9 +3,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -807,7 +807,7 @@ var (
 	ERROR_UNKNOWN                                                                     ErrorCode = "NU000"
 )
 
-func NewNebulaError(code ErrorCode, format string, args ...interface{}) error {
+func NewNebulaError(code ErrorCode, format string, args ...any) error {
 	r := &NebulaError{
 		err:       goerr.New(fmt.Sprintf(format, args...)),
 		errorCode: code,
